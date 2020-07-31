9f7dd066795bb62049cdb3268b24166842785dd5
yaacoubyaacoub <67503602+yaacoubyaacoub@users.noreply.github.com>
2020-08-01 00:13:37 +0300

    Add files via upload

---
+++ Assignment 9/Meal Finder/script.js
@@ -0,0 +1,121 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+const mealsEl = document.getElementById('meals');
+const resultHeading = document.getElementById('result-heading');
+const single_mealEl = document.getElementById('single-meal');
+
+// Search meal and fetch from API
+function searchMeal(e) {
+    e.preventDefault();
+
+    // Clear single meal
+    single_mealEl.innerHTML = '';
+
+    // Get seach term
+    const term = search.value;
+
+    // Check for empty
+    if(term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
+        
+            if(data.meals == null) {
+                resultHeading.innerHTML = `<p>TThere are ni seach results. Try again!</p>`
+            } else {
+                mealsEl.innerHTML = data.meals.map(meal =>`
+                <div class="meal">
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                    <div class="meal-info" data-mealID="${meal.idMeal}">
+                        <h3>${meal.strMeal}</h3>
+                    </div>
+                </div>
+                `).join('');
+            }
+        });
+        // Clear search text
+        search.value = '';
+    } else {
+        alert('please enter a search term');
+    }
+}
+
+// Fetch meal by Id
+function getMealByID(mealID) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+        
+        addMealToDOM(meal);
+    });
+}
+
+// Fetcj random meal
+function getRandomMeal() {
+    // clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+        
+        addMealToDOM(meal);
+    });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+    console.log('1');
+    const ingredients = [];
+
+    for(let i = 1; i <= 20; i++) {
+        if(meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        } else {
+            break;
+        }
+    }
+
+    single_mealEl.innerHTML = `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+            <div class="single-meal-info">
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients</h2>
+                <ul>
+                    ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+                </ul>
+            </div>
+        </div>
+        `;
+}
+
+// Event Listeners
+submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
+mealsEl.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => {
+        if(item.classList) {
+            return item.classList.contains('meal-info');
+        } else {
+            return false;
+        }
+    });
+
+    if(mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealid');
+        getMealByID(mealID);
+    }
+});
